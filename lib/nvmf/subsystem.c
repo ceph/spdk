0e13ec128397d1f43298db0fcb9ee8434327ff7a
Leonid Chernin <leonidc@il.ibm.com>
2025-04-07 10:56:12 +0300

    nvmf: introduced NS reservation for host multipath.
          The new code handles reservation when nvme host
          is connected to different nvmeof GWs.
          Each nvmeof GW starts spdk for handling nvmf-tgt.
          The feature is implemented partually in new spdk
          file  that redefines handlers of reservation
          'load' and 'update' operations.
          Operations related specifically to rbd device are
          called from the new spdk file via commonly used
          bdev->fn_table and handled by bdev_rbd module.
    
          Required:
          1. spdk reservation logic not changed. Just
          reservation ops (load, update) are redirected to
          other file
          2.reservation info is stored in some
          persistent place per namespace - rbd image metadata.
          3.Change of NS reservation in one of the GWs
          should trigger loading of reservation in other
          GW's spdk modules (rbd watcher logic).
    
          Added component 'reservation' for logging
          check-format script was run
    
    Change-Id: I5b187c6ac5eab123fb488c0e6030a668272e81f0
    Signed-off-by: Leonid Chernin <leonidc@il.ibm.com>
    (cherry picked from commit 382b5684abd018c6a3259294d02109449890e96b)
    (cherry picked from commit 6edd5fdd8a2aa316043ea78d9eec8b882d71199e)
    (cherry picked from commit 8436164d7ce42a2d234353cbe9063557b7f7df5f)

--- lib/nvmf/subsystem.c
+++ lib/nvmf/subsystem.c
@@ -27,7 +27,7 @@
 #include "spdk/log.h"
 #include "spdk_internal/utf.h"
 #include "spdk_internal/usdt.h"
-
+#include "nvmf_reservation.h"
 #define MODEL_NUMBER_DEFAULT "SPDK bdev Controller"
 #define NVMF_SUBSYSTEM_DEFAULT_NAMESPACES 32
 
@@ -362,7 +362,6 @@ spdk_nvmf_subsystem_create_ext(struct spdk_nvmf_tgt *tgt,
 		SPDK_ERRLOG("Subsystem memory allocation failed\n");
 		return NULL;
 	}
-
 	subsystem->thread = spdk_get_thread();
 	subsystem->state = SPDK_NVMF_SUBSYSTEM_INACTIVE;
 	subsystem->tgt = tgt;
@@ -2154,7 +2153,7 @@ nvmf_subsystem_ns_changed(struct spdk_nvmf_subsystem *subsystem, uint32_t nsid)
 	}
 }
 
-static uint32_t nvmf_ns_reservation_clear_all_registrants(struct spdk_nvmf_ns *ns);
+uint32_t nvmf_ns_reservation_clear_all_registrants(struct spdk_nvmf_ns *ns);
 
 int
 spdk_nvmf_subsystem_remove_ns(struct spdk_nvmf_subsystem *subsystem, uint32_t nsid)
@@ -2460,8 +2459,8 @@ static int nvmf_ns_reservation_ptpl_store(const struct spdk_nvmf_ns *ns,
 		const struct spdk_nvmf_reservation_info *info);
 static int nvmf_ns_reservation_ptpl_load(const struct spdk_nvmf_ns *ns,
 		struct spdk_nvmf_reservation_info *info);
-static int nvmf_ns_reservation_restore(struct spdk_nvmf_ns *ns,
-				       struct spdk_nvmf_reservation_info *info);
+int nvmf_ns_reservation_restore(struct spdk_nvmf_ns *ns,
+		struct spdk_nvmf_reservation_info *info);
 
 bool
 nvmf_subsystem_zone_append_supported(struct spdk_nvmf_subsystem *subsystem)
@@ -2665,7 +2664,7 @@ spdk_nvmf_subsystem_add_ns_ext(struct spdk_nvmf_subsystem *subsystem, const char
 			goto err;
 		}
 	}
-
+	spdk_try_rbd_reservation_ops_set(ns->bdev);
 	if (nvmf_ns_is_ptpl_capable(ns)) {
 		rc = nvmf_ns_reservation_ptpl_load(ns, &info);
 		if (rc) {
@@ -3137,25 +3136,6 @@ spdk_nvmf_subsystem_get_max_cntlid(const struct spdk_nvmf_subsystem *subsystem)
 	return subsystem->max_cntlid;
 }
 
-struct _nvmf_ns_registrant {
-	uint64_t		rkey;
-	char			*host_uuid;
-};
-
-struct _nvmf_ns_registrants {
-	size_t				num_regs;
-	struct _nvmf_ns_registrant	reg[SPDK_NVMF_MAX_NUM_REGISTRANTS];
-};
-
-struct _nvmf_ns_reservation {
-	bool					ptpl_activated;
-	enum spdk_nvme_reservation_type		rtype;
-	uint64_t				crkey;
-	char					*bdev_uuid;
-	char					*holder_uuid;
-	struct _nvmf_ns_registrants		regs;
-};
-
 static const struct spdk_json_object_decoder nvmf_ns_pr_reg_decoders[] = {
 	{"rkey", offsetof(struct _nvmf_ns_registrant, rkey), spdk_json_decode_uint64},
 	{"host_uuid", offsetof(struct _nvmf_ns_registrant, host_uuid), spdk_json_decode_string},
@@ -3274,7 +3254,7 @@ exit:
 
 static bool nvmf_ns_reservation_all_registrants_type(struct spdk_nvmf_ns *ns);
 
-static int
+int
 nvmf_ns_reservation_restore(struct spdk_nvmf_ns *ns, struct spdk_nvmf_reservation_info *info)
 {
 	uint32_t i;
@@ -3688,7 +3668,7 @@ nvmf_ns_reservation_remove_all_other_registrants(struct spdk_nvmf_ns *ns,
 	return count;
 }
 
-static uint32_t
+uint32_t
 nvmf_ns_reservation_clear_all_registrants(struct spdk_nvmf_ns *ns)
 {
 	struct spdk_nvmf_registrant *reg, *reg_tmp;
