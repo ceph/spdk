bb8a38bdb5dd820f7d62b5656db8db13a2277651
Gil Bregman <gbregman@il.ibm.com>
2025-12-09 23:46:50 +0200

    Update commit hash in version string.
    Signed-off-by: Gil Bregman <gbregman@il.ibm.com>
    (cherry picked from commit 3acfebd1db18209e87fdee39cd967070d08eae14)

--- mk/spdk.common.mk
+++ mk/spdk.common.mk
@@ -332,7 +332,7 @@ LDFLAGS += -fsanitize=fuzzer-no-link
 SYS_LIBS += $(CONFIG_FUZZER_LIB)
 endif
 
-SPDK_GIT_COMMIT := $(shell git rev-parse --short HEAD 2>/dev/null)
+SPDK_GIT_COMMIT := 18269ecbbd6bc94788ccf0bcf0413e554cc5c398
 ifneq (, $(SPDK_GIT_COMMIT))
 COMMON_CFLAGS += -DSPDK_GIT_COMMIT=$(SPDK_GIT_COMMIT)
 endif
