28ac037118cd56ef07f135275903355277047694
Gil Bregman <gbregman@il.ibm.com>
2026-06-17 13:45:15 +0300

    Implement a NULL bdev with no IO support.
    Signed-off-by: Gil Bregman <gbregman@il.ibm.com>

--- module/bdev/null/bdev_null.h
+++ module/bdev/null/bdev_null.h
@@ -28,6 +28,7 @@ struct null_bdev_opts {
 	enum spdk_dif_type dif_type;
 	bool dif_is_head_of_md;
 	enum spdk_dif_pi_format dif_pi_format;
+	bool fail_io;
 };
 
 int bdev_null_create(struct spdk_bdev **bdev, const struct null_bdev_opts *opts);
